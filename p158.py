251ef7707510a74bafba7e501fccc77b36c3a4a7
jaudielalejandro <143145242+jaudielalejandro@users.noreply.github.com>
2025-03-16 16:55:24 -0600

    avance

---
+++ p158.py
@@ -0,0 +1,12 @@
+from os import strerror
+
+try:
+	file = open('newtext.txt', 'wt') # Un nuevo archivo (newtext.txt) es creado.
+	for i in range(10):
+		s = "línea #" + str(i+1) + "\n"
+		for char in s:
+			file.write(char)
+	file.close()
+except IOError as e:
+	print("Se produjo un error de E/S: ", strerror(e.errno))
+    
